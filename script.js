b04786fdf67c7670f357d00dbc3577ac55034799
iMakeStuff <105526428+iMakeStuff69420@users.noreply.github.com>
2023-02-16 16:43:16 -0500

    Fixed some things
    Made the code better

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function showMessage() {
-  var password = prompt("Enter the password:");
-  if (password == "password") {
-    document.getElementById("secret-message").style.display = "block";
+function checkPassword() {
+  var password = document.getElementById("password").value;
+
+  if (password == "1234") {
+    document.getElementById("message").style.display = "block";
+  } else {
+    alert("Incorrect password. Please try again.");
   }
 }
